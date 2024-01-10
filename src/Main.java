72daf131cb99066f3cf74938585acc20e86798ac
jtfrostyboy <jthoma10@hotmail.com>
2024-01-10 13:55:53 -0600

    Layout

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in); // Scanner Object
+        System.out.println("Enter Number");
+
+        int value1 = sc.nextInt(); // Record First Number inputted
+        System.out.println("Select a Math Operation");
+
+        System.out.println("Enter Number");
+        int value2 = sc.nextInt();
+
+        System.out.println("Your numbers are " + value1 + " and " + value2);
     }
 }
+
